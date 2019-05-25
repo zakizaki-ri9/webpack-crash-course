aa595960a1e08671652c40d046d22e44ac2bd507
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-05-25 10:24:26 +0900

    feat: png style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './style.css'
+import './style.scss'
+import zaki from './zaki2.png'
+
+const image = new Image()
+image.src = zaki
+
+document.body.appendChild(image)
+document.body.classList.add('background')
 
 ReactDOM.render(
   <div>
